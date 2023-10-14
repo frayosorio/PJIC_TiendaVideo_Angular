848cf88f3b8300e483dd494bfcf3c3f02658794b
Fray León Osorio Rivera <frayosorio@gmail.com>
2023-10-13 19:31:04 -0500

    agregando modulo EMPRESAS

---
+++ src/app/componentes/empresas/empresas.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmpresasComponent } from './paginas/empresas/empresas.component';
+
+
+
+@NgModule({
+  declarations: [
+    EmpresasComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class EmpresasModule { }
